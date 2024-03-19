ef3d47f33441ee7bb3a3fd714fb68c089b54fb32
dem1d <d3m1doff@gmail.com>
2024-03-19 16:44:36 +0300

    Новая задача на поиск индекса в массиве

--- main.py
+++ main.py
@@ -19,3 +19,26 @@ def ilya_solution(value):
         else:
             used_chars.append(value[i])
     return output
+
+
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+# Задание : программа должна перевести римские цифры в обычные десятичные в формате int.
+# Например :
+# "III" == 3
+# "LVIII" == 58
+# "MCMXCIV" == 1994
+# @roman - римское число
+def roman_to_int(roman):
+    pass
+
+
+# Дан отсортированный массив целых чисел nums и число target. Функция должна вернуть индекс
+# числа target в массиве, а если этого числа нет, то индекс предпологаемого места в массиве.
+def search_insert_position(nums, target):
+    pass
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from main import ilya_solution
+from main import ilya_solution, roman_to_int, search_insert_position
 import unittest
 
 
@@ -11,6 +11,24 @@ class TestStringMethods(unittest.TestCase):
 
     def test_find_longest_substring_third(self):
         self.assertEqual(ilya_solution("abcabad"), "abc")
+
+    def test_roman_to_int_first(self):
+        self.assertEqual(roman_to_int("III"), 3)
+
+    def test_roman_to_int_second(self):
+        self.assertEqual(roman_to_int("LVIII"), 58)
+
+    def test_roman_to_int_third(self):
+        self.assertEqual(roman_to_int("MCMXCIV"), 1994)
+
+    def test_search_insert_position_first(self):
+        self.assertEqual(search_insert_position([1, 3, 5, 7], 3), 1)
+
+    def test_search_insert_position_second(self):
+        self.assertEqual(search_insert_position([1, 3, 5, 7], 4), 2)
+
+    def test_search_insert_position_third(self):
+        self.assertEqual(search_insert_position([1, 3, 5, 7], 12), 4)
 
 
 if __name__ == '__main__':
