0664dcac5d600a75f664dbc61fa89a48722d27d9
dem1d <d3m1doff@gmail.com>
2024-03-19 16:47:05 +0300

    удалил предыдущую задачу

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from main import ilya_solution, roman_to_int, search_insert_position
+from main import ilya_solution, search_insert_position
 import unittest
 
 
@@ -11,15 +11,6 @@ class TestStringMethods(unittest.TestCase):
 
     def test_find_longest_substring_third(self):
         self.assertEqual(ilya_solution("abcabad"), "abc")
-
-    def test_roman_to_int_first(self):
-        self.assertEqual(roman_to_int("III"), 3)
-
-    def test_roman_to_int_second(self):
-        self.assertEqual(roman_to_int("LVIII"), 58)
-
-    def test_roman_to_int_third(self):
-        self.assertEqual(roman_to_int("MCMXCIV"), 1994)
 
     def test_search_insert_position_first(self):
         self.assertEqual(search_insert_position([1, 3, 5, 7], 3), 1)
