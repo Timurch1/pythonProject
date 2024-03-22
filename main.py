fc3fd0153d54829eb4d924cb4244e66c606c76c6
Timur <polyanskiy.tima@gmail.com>
2024-03-22 18:39:39 +0300

    Задача на поиск цифр в строке)))

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-# Нахождение количества определенных символов в строке
-
-def search_symwal(stroka,symwal: str):
+# Нахождение числа цифр в строке
+def to_int_stroka(nums: str) -> int:
     res = 0
-    for i in stroka:
-        if i== symwal:
+    for i in nums:
+         if i == '0':
             res += 1
-    print("Количество",symwal,':', res)
-    return stroka
-
-stroka= str(input("Введите слово: "))
-symwal = str(input("Введите символ: "))
-count = search_symwal(stroka, symwal)
-print(count)
+         elif i == '2':
+             res += 1
+         elif i == '3':
+             res += 1
+         elif i == '4':
+             res += 1
+         elif i == '5':
+             res += 1
+         elif i == '6':
+             res += 1
+         elif i == '7':
+             res += 1
+         elif i == '8':
+             res += 1
+         elif i == '9':
+             res += 1
+    print("Количество цифр: ", res)
+    return nums
+stroka = input("Введите слово: ")
+count = to_int_stroka(stroka)
+print('Наше слово:', count)
