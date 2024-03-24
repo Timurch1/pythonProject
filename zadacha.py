c8b14d287b002e1e99372f7f5a5c1db10e12ae8e
Timur <polyanskiy.tima@gmail.com>
2024-03-24 23:22:04 +0300

    New zadacha)

--- zadacha.py
+++ zadacha.py
@@ -14,3 +14,16 @@ def to_often(nums: str):
 
 strochka = input("Введите слово: ")
 to_often(strochka)
+
+
+
+# Удаление повторяющихся элементов и нахождения числа неповторящихся элементов)
+def to_set(nums: list):
+   nums = set(nums)
+   for i in range(len(nums)):
+       i += 1
+   print("Результат: ", i, end=', ')
+   return nums
+
+it_list = [1, 1, 1, 2, 2, 3, 4, 4, 4, 5, 10, 10, 15, 18, 18, 20]
+print(to_set(it_list))
